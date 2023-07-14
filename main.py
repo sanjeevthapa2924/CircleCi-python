aa4f2a9094c0034c0c1fe8359f81be184788b73f
Sanjeev Thapa <sanjeevthapa2924@gmail.com>
2023-07-14 07:39:58 +0545

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def say_hello(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    name = 'TrainWithShubham'
+    name = 'this is sanjeev thapa ... and hi'
     say_hello(name)
     up = to_upper(name)
     print(up)
